c70f59d11aa08a680d6ca1c826775b8fe49d7abc
Neto Valcacer <102036137+cvalcacer@users.noreply.github.com>
2022-04-12 22:35:11 -0300

    Add files via upload
    First files for Threads.

---
+++ Projeto16_Threads/src/br/com/impacta/threads/ContadorThread.java
@@ -0,0 +1,47 @@
+package br.com.impacta.threads;
+
+public class ContadorThread extends Thread {
+	
+	
+
+	@Override
+	public void run() {
+		
+		try {
+			
+			for (int i = 0; i < 20; i++) {
+				System.out.println("*");
+				Thread.sleep(500);
+			}
+			
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		super.run();
+	}
+	
+	
+
+}
